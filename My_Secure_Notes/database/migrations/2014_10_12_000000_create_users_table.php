67a690ca46af2ed834def1b4dfcb145d122a2765
ildafm <ildafm4000@gmail.com>
2023-12-06 09:33:44 +0700

    Breeze install

--- My_Secure_Notes/database/migrations/2014_10_12_000000_create_users_table.php
+++ My_Secure_Notes/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes(); // Tambahkan ini untuk soft delete
         });
     }
 
