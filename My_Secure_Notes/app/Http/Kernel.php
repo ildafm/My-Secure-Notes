c7d34011609d32318480795675e2a73e3e390d77
ildafm <ildafm4000@gmail.com>
2023-12-06 10:40:47 +0700

    Register dan Verifikasi Email
    -> aplikasi bisa register
    -> aplikasi membutuhkan verifikasi email untuk dapat masuk ke tampilan dashboard

--- My_Secure_Notes/app/Http/Kernel.php
+++ My_Secure_Notes/app/Http/Kernel.php
@@ -36,7 +36,9 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         ],
 
         'api' => [
--- My_Secure_Notes/routes/web.php
+++ My_Secure_Notes/routes/web.php
@@ -17,8 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Route::get('/register', [RegisteredUserController::class, 'create'])
+//     ->middleware('guest')
+//     ->name('register');
+
+// Route::post('/register', [RegisteredUserController::class, 'store'])
+//     ->middleware('guest');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
